d61e56ff509ee1fe0e867c04c26e03d1ca24c9a2
Mendy <mendybeltcohen@gmail.com>
2018-08-12 21:36:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,12 @@
 #Add  code here!
 
-# def prime?(num)
-#   if num % num === 0
-#     true
-#   elsif num % num ===
-#     false
-#   end
-# end 
-
 def prime?(num)
-  while num % num === 0
-   true
- end
+  if num % num === 0
+    true
+  elsif num % num ===
+    false
+  end
+end 
+
   
-end  
 
